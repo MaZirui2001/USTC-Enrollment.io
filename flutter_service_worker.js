ae03c1477de7892fc30bddd4495bf60ec65bfd73
madrid <454434635@qq.com>
2024-06-20 19:23:04 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,8 +9,8 @@ const RESOURCES = {"manifest.json": "a874d6674d803366c7ea9616cbf6dc68",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"index.html": "7e5fd696a018441fb1d6e74bf2467556",
-"/": "7e5fd696a018441fb1d6e74bf2467556",
+"index.html": "1d1d0940d71c29d9ca82d7fba0e9744f",
+"/": "1d1d0940d71c29d9ca82d7fba0e9744f",
 "version.json": "42546b29b79fcb508ce183ba9ccc8ea1",
 "canvaskit/canvaskit.js": "738255d00768497e86aa4ca510cce1e1",
 "canvaskit/skwasm.wasm": "4051bfc27ba29bf420d17aa0c3a98bce",
@@ -32,9 +32,9 @@ const RESOURCES = {"manifest.json": "a874d6674d803366c7ea9616cbf6dc68",
 "assets/fonts/MaterialIcons-Regular.otf": "af02512893c29457a94b7c651e9bda37",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/FontManifest.json": "e5b7d4d66ddaaf9a896f27a6e2bf99aa",
-"flutter_bootstrap.js": "ebbdad6bdcc0e51d5042529d4a6b206c",
+"flutter_bootstrap.js": "77bdc4425b105854cde3d6a982d2a829",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "af56d810ce7993cc19c8b4571effa259"};
+"main.dart.js": "617065be46ed6e0b9bfaea47c15251d2"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
